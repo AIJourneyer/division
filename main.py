7c1b59b3a6fec7ac805a60795f0a9cceb4b11afe
Sys1234 <adil.s@way2news.com>
2025-06-12 16:18:09 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def divide_numbers(request):
+    request_args = request.args
+    num1 = float(request_args.get('num1', 1))
+    num2 = float(request_args.get('num2', 1))
+
+    if num2 == 0:
+        return {"error": "Division by zero is not allowed."}
+
+    return {
+        'num1': num1,
+        'num2': num2,
+        'result': num1 / num2
+    }
